2efac9f318ba08dcbb078079d204623ecc8431f2
Alyssa Levitz <alyssal@yammer-inc.com>
2013-09-08 16:58:54 -0700

    topics scaffolding + setting the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Suggestotron2::Application.routes.draw do
+  resources :topics
+
+  root 'topics#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
