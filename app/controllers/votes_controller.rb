ebe6e3ac8dec8ed38b468925bbb8e43483074ae4
Alyssa Levitz <alyssal@yammer-inc.com>
2013-09-10 18:59:22 -0700

    got the redirect working the way i want for voting.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,12 @@ class VotesController < ApplicationController
     topic = Topic.find(params[:topic_id])
     vote = topic.votes.build
     vote.save!
-    redirect_to(topics_path)
+
+    redirect_to :back
+    
+	    # redirect_to(topic_path(params[:topic_id]))
+    	# redirect_to(topics_path)
+    
   end	
 
 end
